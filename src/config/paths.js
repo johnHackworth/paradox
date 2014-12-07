8f8142f0037e6986c923f502bd41e3538c414621
johnHackworth <javieralvarezlop@gmail.com>
2014-12-07 17:01:50 +0100

    fixed config

--- src/config/paths.js
+++ src/config/paths.js
@@ -1,7 +1,7 @@
 window.pdx.namespace('pdx.config.paths');
 
 pdx.config.paths = {
-  urlBase: 'http://johnhackworth.github.com/paradox',
+  urlBase: 'http://johnhackworth.github.io/paradox',
   newsList: 'http://paradoxtest.herokuapp.com/feed',
   newsItem: 'http://paradoxtest.herokuapp.com/item'
 };
